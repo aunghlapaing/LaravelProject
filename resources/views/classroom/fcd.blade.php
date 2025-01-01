804b0023a0563f2849cb8b7057983b582ec669b7
aunghlapaing <aunghlapaing8949@gmail.com>
2025-01-01 14:34:22 +0700

    add route

---
+++ resources/views/classroom/fcd.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel || Route Testing</title>
+</head>
+<body>
+    <h1>Laravel</h1>
+    <hr>
+    {{ url('/register/user') }}
+    <hr>
+    {{ url('/register/user/aunghla') }}
+    
+</body>
+</html>
